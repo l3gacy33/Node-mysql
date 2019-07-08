be37dd04e1ac23b27758d07d0e106efa81aabd1f
Jordan <jtfranklin04203@gmail.com>
2019-07-08 10:03:03 -0400

    connected mysql driver to my database

--- API.js
+++ API.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mysql   = require('mysql');
+
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin',
+  password : 'student',
+});
+
+db.connect(function(err){
+    if (err) throw err
+    console.log("Mysql is connected looking good!")
+});
+
 const API = express();
 
-
-
-API.listen(5000)
-
-console.log("your sevrer is now up on port 5000")
+API.listen(5000);
+console.log("your sevrer is now up on port 5000");
 
 
 
