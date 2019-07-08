96263fba696594a95f33875763a355021ab9dc21
Jordan <jtfranklin04203@gmail.com>
2019-07-08 13:30:02 -0400

    learning how to create a database, tables from the coding part instead of the terminal and learned how to post, add, and delete as well

--- API.js
+++ API.js
@@ -41,6 +41,73 @@ API.get("/createposttable", (req, res) => {
     });
 })
 
+// inserts a post into posts table
+API.get("/addpost", (req, res) => {
+    let post = {title: "My first post", body: "hello today was an interesting day"}
+    let sql ='INSERT INTO posts SET ?';
+    db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('first post added ..')
+    });
+})
+
+// adding second post
+API.get("/secondpost", (req, res) => {
+    let post = {title: "My second post", body: "well the food for today was different from the other times"}
+    let sql ='INSERT INTO posts SET ?';
+    db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('second post added ..')
+    });
+})
+
+// adding a third post
+API.get("/thirdpost", (req, res) => {
+    let post = {title: "My third post", body: "today's lesson was easy and reall quick to understand"}
+    let sql ='INSERT INTO posts SET ?';
+    db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('third post added ..')
+    });
+})
+
+
+
+
+// select all post
+API.get("/getpost", (req, res) => {
+    let sql ='SELECT * FROM posts';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+})
+
+
+// select a single post
+API.get("/getpost/:id", (req, res) => {
+    let sql ='SELECT * FROM posts WHERE ID =' + req.params.id;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+})
+
+
+// delete a post
+API.get("/deletepost/:id", (req, res) => {
+    let sql ='DELETE FROM posts WHERE ID =' + req.params.id;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+})
 
 
 
