d4e9c8d45dd9e42639327ff806599089bc61df2b
Jordan <jtfranklin04203@gmail.com>
2019-07-08 11:51:48 -0400

    making this code able to make a database inside the terminal without going into the terminal

--- API.js
+++ API.js
@@ -5,6 +5,7 @@ const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'admin',
   password : 'student',
+  database : 'blog'
 });
 
 db.connect(function(err){
@@ -14,26 +15,37 @@ db.connect(function(err){
 
 const API = express();
 
+// creating db
+API.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE blog'
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database created ... ');
+    });
+});
+
+// create a table
+API.get("/createposttable", (req, res) => {
+    // create table called posts
+    // have an id
+    // Auto increment
+    // title
+    // body
+    // primary key will be id
+    let sql = 'CREATE TABLE posts(ID int NOT NULL AUTO_INCREMENT, title varchar(255), body varchar(255),PRIMARY KEY (ID) );'
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Table was created')
+    });
+})
+
+
+
+
+
 API.listen(5000);
 console.log("your sevrer is now up on port 5000");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
